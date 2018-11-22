5ddebb500aa7290a80d21ea7be75230a6f779359
eduardkarnaukh <eduard.karnaukh@gmail.com>
2018-11-22 19:39:19 +0100

    design

---
+++ Block/Adminhtml/System/Config/Info.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eduardkarnaukh
+ * Date: 21/11/2018
+ * Time: 23:50
+ */
+
+namespace Magetoolkit\Base\Block\Adminhtml\System\Config;
+
+
+class Info extends \Magento\Config\Block\System\Config\Form\Fieldset
+{
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        return '<iframe id="magetoolkit" width="100%" src="http://magento-last.loc/info/index.html" ></iframe>';
+    }
+}
