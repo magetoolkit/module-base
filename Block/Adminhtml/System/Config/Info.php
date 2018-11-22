9993932dbbd69276a4b0bb0e6140a965fd17f424
MageToolKit <info@magetoolkit.com>
2018-11-22 23:45:41 +0100

    Update Info.php

--- Block/Adminhtml/System/Config/Info.php
+++ Block/Adminhtml/System/Config/Info.php
@@ -11,8 +11,21 @@ namespace Magetoolkit\Base\Block\Adminhtml\System\Config;
 
 class Info extends \Magento\Config\Block\System\Config\Form\Fieldset
 {
+    private $storeManager;
+    public function __construct(
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Backend\Block\Context $context,
+        \Magento\Backend\Model\Auth\Session $authSession,
+        \Magento\Framework\View\Helper\Js $jsHelper,
+        array $data = []
+    )
+    {
+        $this->storeManager = $storeManager;
+        parent::__construct($context, $authSession, $jsHelper, $data);
+    }
+
     public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
-        return '<iframe id="magetoolkit" width="100%" src="http://www.magetoolkit.com/info/index.html?id=' . uniqid() .'" ></iframe>';
+        return '<iframe id="magetoolkit" width="100%" src="http://magento-last.loc/info/index.html?id=' . $this->storeManager->getStore()->getBaseUrl() .'" ></iframe>';
     }
 }
